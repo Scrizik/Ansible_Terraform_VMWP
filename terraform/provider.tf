5cd9eee93d5f3bc9f075305871dbaff229af05dd
Scrizik <scrizik@gmail.com>
2026-02-17 19:07:40 +0100

    Feat: Suppresion des fonctionnalités VMWare

--- terraform/provider.tf
+++
@@ -1,7 +0,0 @@
-provider "vmworkstation" {
-  user     = "jordan"
-  password = "Serveur1234!"  # Remplace par ton mot de passe
-  url      = "http://localhost:8697/api"
-  https    = false
-  debug    = true
-}
